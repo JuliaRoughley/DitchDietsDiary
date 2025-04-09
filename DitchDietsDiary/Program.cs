18b4f2835adb5615566d09e7f591e226615a9f00
julia <roughleyjulia@gmail.com>
2025-04-09 13:31:17 +0100

    Registered in the DI container

--- DitchDietsDiary/Program.cs
+++ DitchDietsDiary/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using DitchDietsDiary.Infrastructure.Repositories;
+using DitchDietsDiary.Core.Interfaces;
+using DitchDietsDiary.Infrastructure.Data; 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
 // Add services to the container.
+builder.Services.AddScoped<IFoodLoggingRepository, FoodLoggingRepository>();
+
+// Register the DbContext to use an in-memory database
+builder.Services.AddDbContext<FoodLoggingDbContext>(options =>
+    options.UseInMemoryDatabase("FoodLoggingDb")); // Use in-memory database
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
